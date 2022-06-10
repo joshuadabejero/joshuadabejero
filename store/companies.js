727e2790d32bc9989d9c2788a9e1cdf6105569fc
Joshua Dawi Abejero <joshuadabejero@gmail.com>
2022-06-10 15:07:46 +0800

    feat: add home credit in store/companies.js

--- store/companies.js
+++ store/companies.js
@@ -1,8 +1,15 @@
 export const state = () => ({
   companies: [
     {
+      name: 'Home Credit Philippines',
+      date: 'August 2021 - Present',
+      description:
+        'Responsible in building and maintaining of Home Credit Market Place (MPL). Development of new software according to different business requirements. Ensure quality of deliverables by adhering to project standards. Perform code review of programs written by peers. Perform unit and system integration testing. Resolve complex system bugs and issues identified during all phases of testing. Monitoring and configuration of deployed applications. Maintenance of deployed applications according to necessary changes.',
+      position: 'IT Web Developer',
+    },
+    {
       name: 'Blue Sky Trading Co., Inc.',
-      date: 'August 2018 - Present',
+      date: 'August 2018 - July 2021',
       description:
         'Maintaining a Human Resource Information System with a Payroll System which using a Struts 2 Framework. Creating a custom report system based on client’s wants and needs. Using Jasper iReports in building reports, then using Java as it’s backend programming language and JSP for its frontend.',
       position: 'Java Web Developer',
