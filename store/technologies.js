c06c7955a8c7517dce909f1b33dd4c11411e8da9
Joshua Dawi Abejero <joshuadabejero@gmail.com>
2022-06-10 14:46:48 +0800

    feat: add data as store data in nuxt store or vuex store

---
+++ store/technologies.js
@@ -0,0 +1,169 @@
+export const state = () => ({
+  technologies: [
+    {
+      name: '',
+      description: 'ES7',
+      icon: 'mdi-language-javascript',
+    },
+    {
+      name: '',
+      description: 'VueJs',
+      icon: 'mdi-vuejs',
+    },
+    {
+      name: 'Vuex',
+      description: 'Vuex',
+      icon: '',
+    },
+    {
+      name: 'Jekyll',
+      description: 'Jekyll',
+      icon: '',
+    },
+    {
+      name: 'Liquid',
+      description: 'Liquid',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'Bootstrap',
+      icon: 'mdi-bootstrap',
+    },
+    {
+      name: '',
+      description: 'Sass',
+      icon: 'mdi-sass',
+    },
+    {
+      name: '',
+      description: 'Java',
+      icon: 'mdi-language-java',
+    },
+    {
+      name: '',
+      description: 'C#',
+      icon: 'mdi-language-csharp',
+    },
+    {
+      name: 'VB.Net',
+      description: 'VB.Net',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'PHP',
+      icon: 'mdi-language-php',
+    },
+    {
+      name: '',
+      description: 'NodeJs',
+      icon: 'mdi-nodejs',
+    },
+    {
+      name: '',
+      description: 'Firebase',
+      icon: 'mdi-firebase',
+    },
+    {
+      name: '',
+      description: 'Laravel',
+      icon: 'mdi-laravel',
+    },
+    {
+      name: 'Netlify',
+      description: 'Netlify',
+      icon: '',
+    },
+    {
+      name: 'MSSQL',
+      description: 'MSSQL',
+      icon: '',
+    },
+    {
+      name: 'MYSQL',
+      description: 'MYSQL',
+      icon: '',
+    },
+    {
+      name: 'MONGO',
+      description: 'MONGODB',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'JSON',
+      icon: 'mdi-cloud-braces',
+    },
+    {
+      name: '',
+      description: 'AWS',
+      icon: 'mdi-aws',
+    },
+    {
+      name: 'Apache',
+      description: 'Apache',
+      icon: '',
+    },
+    {
+      name: 'Tomcat',
+      description: 'Tomcat',
+      icon: '',
+    },
+    {
+      name: 'Axios',
+      description: 'Axios',
+      icon: '',
+    },
+    {
+      name: 'Struts2',
+      description: 'Struts2',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'WordPress',
+      icon: 'mdi-wordpress',
+    },
+    {
+      name: 'Woo',
+      description: 'WooCommerce',
+      icon: '',
+    },
+    {
+      name: 'Ps',
+      description: 'Adobe Photoshop',
+      icon: '',
+    },
+    {
+      name: 'Ai',
+      description: 'Adobe Illustrator',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'Basic Linux Command',
+      icon: 'mdi-console-line',
+    },
+    {
+      name: 'Circle CI',
+      description: 'Circle CI',
+      icon: '',
+    },
+    {
+      name: 'PHPUnit',
+      description: 'PHPUnit',
+      icon: '',
+    },
+    {
+      name: '',
+      description: 'Git',
+      icon: 'mdi-git',
+    },
+    {
+      name: 'Moz SEO',
+      description: 'Moz SEO',
+      icon: '',
+    },
+  ],
+})
