1f4043adac0f6096ef1946501dabefe91723e3d8
joshuadabejero <joshuadabejero@gmail.com>
2025-10-03 22:50:42 +0800

    fix: add href for navigation

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -94,11 +94,11 @@
         <div class="footer__quick-links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#technology">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </div>
         <div class="footer__services">
