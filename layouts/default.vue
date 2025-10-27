d54546f65011420228e60e4700bf62f55554e458
joshuadabejero <joshuadabejero@gmail.com>
2025-10-27 11:20:52 +0800

    feat: add meta data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,37 @@
+<script setup>
+useHead({
+  title: "Joshua — Web Developer",
+  meta: [
+    {
+      name: "description",
+      content: "Full Stack Developer specializing in Laravel and Vue.js",
+    },
+
+    { property: "og:type", content: "website" },
+    { property: "og:title", content: "Joshua - Web Developer" },
+    {
+      property: "og:description",
+      content: "Full Stack Developer specializing in Laravel and Vue.js.",
+    },
+    { property: "og:url", content: "https://your-domain.com" },
+    {
+      property: "og:image",
+      content: "https://joshuadabejero.netlify.app/images/og-image.jpg",
+    },
+
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:title", content: "Joshua — Web Developer" },
+    {
+      name: "twitter:description",
+      content: "Full Stack Developer specializing in Laravel and Vue.js.",
+    },
+    {
+      name: "twitter:image",
+      content: "https://joshuadabejero.netlify.app/images/og-image.jpg",
+    },
+  ],
+});
+</script>
 <template>
   <v-app>
     <AppHeader />
